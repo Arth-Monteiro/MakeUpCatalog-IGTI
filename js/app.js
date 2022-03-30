375492ca891d3db4dcf37684e5e685e458fa82b9
Arthur Monteiro <arthur.m.monteiro2902@gmail.com>
2022-03-30 18:49:41 -0300

    Display dos produtos; paginacao e ordenacao por categorias

--- js/app.js
+++ js/app.js
@@ -1,74 +1,146 @@
-//EXEMPLO DO CÓDIGO PARA UM PRODUTO
-function productItem(product) {
-  const name = product['name'];
-  const item = `<div class="product" data-name="${name}" data-brand="nyx" data-type="bronzer" tabindex="508">
-  <figure class="product-figure">
-    <img src="${product['image_link']}" width="215" height="215" alt="${name}" onerror="javascript:this.src='img/unavailable.png'">
-  </figure>
-  <section class="product-description">
-    <h1 class="product-name">${name}</h1>
-    <div class="product-brands"><span class="product-brand background-brand">${product['brand']}</span>
-    <span class="product-brand background-price">R$ ${((+product['price']) * 5.5).toFixed(2)}</span></div>
-  </section>
-  <!--  // CARREGAR OS DETALHES-->
-</div>`;
+// class Products {
+//   constructor() {
+//     let items;
+    
+//     (async function () {
+//       items = await (async () => {
+//         return await fetch("./../data/products.json")
+//         .then(res => res.json())
+//       })();
+
+//       items = items.map(r => {
+//         return {
+//           id: r['id'],
+//           name: r['name'].replace('\n', '').trim() ?? '',
+//           brand: r['brand'] ?? '',
+//           category: r['category'] ?? '',
+//           product_type: r['product_type'] ?? '',
+//           image_link: r['image_link'],
+//           price: +r['price'] * 5.5,
+//           rating: +r['rating'] ?? 0
+//         }
+//       });
+//     })();
+
+//     this.getItems = function () {
+//       return items;
+//     };
+//   }
+// }
+
+function toTitleCase(str) {
+  if (!str) return 'Null';
+  return str.replace(
+    /\w\S*/g,
+    function(txt) {
+      return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+    }
+  );
+}
+
+function setComboOptions(combo, values) {
+  combo  = document.getElementById(combo);
+  values = values.sort();
+
+  values.map(opt => combo.insertAdjacentHTML('beforeend', `<option value=${opt}>${toTitleCase(opt.replace('_', ' '))}</option>`) );
+};
+
+function productItem(product, cont) {
+
+  const item = `
+  <div class="product" data-name="${product.name}" data-brand="${product.brand}" data-type="${product.product_type}" tabindex="${cont}" id="product_${product.id}">
+    <figure class="product-figure">
+      <img src="${product.image_link}" width="215" height="215" alt="${product.name}" onerror="javascript:this.src='img/unavailable.png'">
+    </figure>
+    <section class="product-description">
+      <h1 class="product-name">${product.name}</h1>
+      <div class="product-brands"><span class="product-brand background-brand">${product.brand}</span>
+      <span class="product-brand background-price">R$ ${(+product.price).toFixed(2)}</span></div>
+    </section>
+    <!--  // CARREGAR OS DETALHES-->
+    ${loadDetails(product)}
+  </div>`;
 
 return item;
 }
 
-//EXEMPLO DO CÓDIGO PARA OS DETALHES DE UM PRODUTO
 function loadDetails(product) {
-  let details = `<section class="product-details"><div class="details-row">
-        <div>Brand</div>
+  const details = ['brand', 'price', 'rating', 'category', 'product_type'];
+
+  let section =  Object.entries(product)
+    .filter(([name, value]) => details.includes(name))
+    .map(([name, value]) =>
+      `<div class="details-row">
+        <div>${name}</div>
         <div class="details-bar">
-          <div class="details-bar-bg" style="width= 250">nyx</div>
+          <div class="details-bar-bg" style="width= 250">${value}</div>
         </div>
-      </div><div class="details-row">
-        <div>Price</div>
-        <div class="details-bar">
-          <div class="details-bar-bg" style="width= 250">10.49</div>
-        </div>
-      </div><div class="details-row">
-        <div>Rating</div>
-        <div class="details-bar">
-          <div class="details-bar-bg" style="width= 250">5</div>
-        </div>
-      </div><div class="details-row">
-        <div>Category</div>
-        <div class="details-bar">
-          <div class="details-bar-bg" style="width= 250"></div>
-        </div>
-      </div><div class="details-row">
-        <div>Product_type</div>
-        <div class="details-bar">
-          <div class="details-bar-bg" style="width= 250">bronzer</div>
-        </div>
-      </div></section>`;
+      </div>`
+    ).join("");
+
+    return `<section class="product-details">${section}</section>`
 }
 
-async function getItems() {
-  return await fetch("./../data/products.json").then(r => r.json());
-}
+function printItems() {
+  const way = sorting.split('_')[0];
+  const field = sorting.split('_')[1];
 
-async function printItems() {
-  const items = await getItems();
+  items.sort((a, b) => {  
+    if (field === 'name') {
+      if ( a[field] < b[field] ) return -1
+      else if ( a[field] > b[field] ) return 1
+      return 0
+    } 
 
-  items.sort((a,b) => {
-    a['rating'] = a['rating'] ?? 0;
-    b['rating'] = b['rating'] ?? 0;
-    
-    return b['rating'] - a['rating'];
+    return a[field] - b[field];
   });
 
-  const catalog = document.getElementById("catalog");
-  for (let item of items.slice(qntd, qntd+20)) {
-    catalog.innerHTML += productItem(item);
+  if (way === 'desc') items.reverse();
+
+  //TODO: Apply filters on items;
+
+  for (let cont = qntd; cont < qntd+20; cont++) {
+    catalog.innerHTML += productItem(items[cont], cont);
   }
-  qntd += 20;
+
+  qntd+=20;
 }
 
+function changeFilter() {
+  qntd = 0;
+  sorting = document.getElementById('sort-type').value;
+  document.getElementById('catalog').innerHTML = "";
+  printItems();
+}
+
+(async () => {
+  items = await fetch("./../data/products.json").then(r => r.json()); 
+  items = items.map(r => {
+    return {
+      id: r['id'],
+      name: toTitleCase(r['name'].replace('\n', '').trim()),
+      brand: toTitleCase(r['brand']),
+      category: toTitleCase(r['category']),
+      product_type: r['product_type'] ?? 'null',
+      image_link: r['image_link'],
+      price: +r['price'] * 5.5,
+      rating: +r['rating'] ?? 0
+    }
+  });
+
+  [
+    ['filter-brand', 'brand'], 
+    ['filter-type', 'product_type']
+  ].forEach(([combo, values]) => setComboOptions(combo, [...new Set(items.map(item => item[values]))]));
+
+  printItems();
+
+})();
+
+let items;
 let qntd = 0;
-printItems();
+let sorting = document.getElementById('sort-type').value;
+const catalog = document.getElementById("catalog");
 
 // Implementação de paginação
 document.addEventListener('scroll', function(event)
@@ -81,27 +153,31 @@ document.addEventListener('scroll', function(event)
     );
     
     const actPos = html.scrollTop || body.scrollTop;
-    if (height - actPos <= html.clientHeight) printItems();
+
+    if (Math.floor(height - actPos) <= html.clientHeight) printItems();
 });
-/*
-Products Keys
-0: "id"
-1: "brand"
-2: "name"
-3: "price"
-4: "price_sign"
-5: "currency"
-6: "image_link"
-7: "product_link"
-8: "website_link"
-9: "description"
-10: "rating"
-11: "category"
-12: "product_type"
-13: "tag_list"
-14: "created_at"
-15: "updated_at"
-16: "product_api_url"
-17: "api_featured_image"
-18: "product_colors"
-*/
+
+
+
+// /*
+// Products Keys
+// 0: "id"
+// 1: "brand"
+// 2: "name"
+// 3: "price"
+// 4: "price_sign"
+// 5: "currency"
+// 6: "image_link"
+// 7: "product_link"
+// 8: "website_link"
+// 9: "description"
+// 10: "rating"
+// 11: "category"
+// 12: "product_type"
+// 13: "tag_list"
+// 14: "created_at"
+// 15: "updated_at"
+// 16: "product_api_url"
+// 17: "api_featured_image"
+// 18: "product_colors"
+// */
